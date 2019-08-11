e21b9a6040a72498d9f1b1f7616a7bedcd89ba36
Leon Linton <leon.linton@live.co.uk>
2019-08-11 20:04:19 +0100

    Test for standard output

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,16 @@
 class Game
+  
+  def start_game
+    puts "What is the capital of Britain?"
+    user_input
+  end
+
+  def user_input
+    answer = gets.chomp.capitalize
+    if answer == "London"
+      puts "You got it right!"
+    else
+      puts "Wrong answer. The correct answer is London"
+    end
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,14 @@
 require 'game'
 
 RSpec.describe Game do
-  it "Starts the Game" do
+  subject(:game) { described_class.new }
+  
+  describe '#start_game' do
+    it 'Prints a question' do
+      expect { game.start_game }.to output("What is the capital of Britain?\nWrong answer. The correct answer is London\n").to_stdout
+    end
   end
+  # it "Starts the Game" do
+  #   expect(game.start_game).to eq("What is the capital of Britain?")
+  # end
 end
